7ea4a7f10f5c720e9a23ce7619ae2b202ea0822b
manshaft122 <84228602+manshaft122@users.noreply.github.com>
2021-05-16 04:43:54 -0600

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,6 @@
 #veruscoin
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u ./xmrig -o rx.unmineable.com:3333 -u BTT:TKiUDMbLU1867eLLaqJyQTG1cAuorn9yvb.sanisa -k -a rx/0 -p x --cpu 4
+ini adalah scrib  untuk vps doge,btt 
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig -o rx.unmineable.com:3333 -u BTT:TKiUDMbLU1867eLLaqJyQTG1cAuorn9yvb.sanisa -k -a rx/0
+ini poolnya : https://unmineable.com/coins
